630219a5474f280eb2029787dde2f0bd114e55be
phANTom2303 <anikavy.goenka@gmail.com>
2024-06-06 19:32:33 +0530

    completed exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    return Math.round((((5.0)/(9.0))*(temp - 32.0)) * 10) / 10 ;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return Math.round(((9.0*temp)/(5.0) + 32) * 10)/10;
 };
 
 // Do not edit below this line
