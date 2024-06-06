d88e04ce97e21e7ed2387f6268bf2aef411c3533
phANTom2303 <anikavy.goenka@gmail.com>
2024-06-06 18:40:14 +0530

    Completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function (word, num) {
+    if (num === -1)
+        return 'ERROR';
+    else if (num === 0)
+        return '';
+    else {
+        let res = '';
+        for (let i = 1; i <= num; i++)
+            res = res + word;
+        return res;
+    }
 
 };
 
