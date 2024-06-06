a136a14a2ceb9469d5a33377671897d5269c992e
phANTom2303 <anikavy.goenka@gmail.com>
2024-06-06 18:45:03 +0530

    finished exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    if (str.length <= 1)
+        return str;
+    else {
+        let rev = '';
+        for(let i = 0; i < str.length;i++)
+            rev = str.charAt(i) + rev;
+        return rev;
+    }
 };
 
 // Do not edit below this line
