a7ba6c2e246929da23e2505a1318b25be2aec579
phANTom2303 <anikavy.goenka@gmail.com>
2024-06-06 19:06:34 +0530

    Completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...dels) {
+    for (const del of dels) {
+        while (arr.indexOf(del) >= 0)
+            arr.splice(arr.indexOf(del), 1);
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
