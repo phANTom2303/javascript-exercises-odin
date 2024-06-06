632b52e3f4b7973f947872d2ae466b83b149ed4d
phANTom2303 <anikavy.goenka@gmail.com>
2024-06-06 19:17:08 +0530

    solved exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    if (typeof (num1) === 'number' && typeof (num2) === 'number') {
+        if (num1 > 0 && num2 > 0) {
+            let sum = 0;
+            let max = (num1 > num2) ? num1 : num2;
+            let min = num1 + num2 - max;
+            for (let i = min; i <= max; i++)
+                sum += i;
+            return sum;
+        }
+        return 'ERROR';
+    }
+    else
+        return 'ERROR';
 };
 
 // Do not edit below this line
