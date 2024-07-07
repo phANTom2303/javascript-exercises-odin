eef3695802c3f7f55e989db8d6d2be40753c0a6e
phANTom2303 <anikavy.goenka@gmail.com>
2024-07-07 22:01:42 +0530

    Completed 09 palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    let forward = "";
+    let reverse = "";
+    for (let i = 0; i < str.length; i++) {
+        let c = str.charAt(i);
+        if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) {
+            forward = forward + c;
+            reverse = c + reverse;
+        }
+    }
+    if (forward == reverse)
+        return true;
+    else 
+        return false;
 };
 
 // Do not edit below this line
