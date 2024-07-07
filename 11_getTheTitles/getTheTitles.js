b4138f31982d30d595d2472c85a9fd539eda2edb
phANTom2303 <anikavy.goenka@gmail.com>
2024-07-07 22:11:24 +0530

    completed 11 get the titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(objArr) {
+    const titles = objArr.map( function (obj){
+        return obj.title;
+    })
+    return titles;
 };
 
 // Do not edit below this line
