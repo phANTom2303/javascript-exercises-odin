447c13f9c2c9729a70393c4fff8334d800789c3f
phANTom2303 <anikavy.goenka@gmail.com>
2024-07-07 22:08:15 +0530

    finished 10 fibinacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    if (typeof num != 'number')
+        num = parseInt(num);
 
+    if (num < 0)
+        return "OOPS";
+    if (num == 0)
+        return 0;
+    else if (num <= 2)
+        return 1;
+    else {
+        let a = 1, b = 1, sum;
+        for (let i = 3; i <= num; i++) {
+            sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
