057fa24ff723bd5f6f6c3642d3c9c2e1eb1db759
phANTom2303 <anikavy.goenka@gmail.com>
2024-07-07 19:54:27 +0530

    Completed 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	if(num1 == 0 && num2 == 0)
+      return 0;
+    else
+      return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = arr.reduce(function(total, num){
+    return total + num;
+  },0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = arr.reduce(function(total, num){
+    return total * num;
+  },1);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num,power) {
+	let ans = 1;
+  for(let i = 1; i <= power;i++)
+      ans = ans * num;
+  return ans;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num == 0)
+      return 1
+	else if(num <= 2)
+      return num;
+  else {
+    let ans = 1;
+    for(let i =1; i <= num; i ++)
+      ans = ans * i;
+    return ans;
+  }
 };
 
 // Do not edit below this line
